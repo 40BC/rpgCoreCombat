974cbac460f97a2df42382f80fa0a73f2f4bad57
Brandon <40BC@users.noreply.github.com>
2022-06-10 22:34:51 -0500

    Patrol Path and Dwelling Time added

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using RPG.Combat;
 using RPG.Core;
 using RPG.Movement;
-using System;
+using UnityEngine.AI;
 
 namespace RPG.Control
 {
@@ -10,62 +10,110 @@ namespace RPG.Control
     {
         [SerializeField] float chaseDistance = 5f;
         [SerializeField] float suspicionTime = 5f;
+        [SerializeField] PatrolPath patrolPath;
+        [SerializeField] float waypointTolerance = 1f;
+        [SerializeField] float attackSpeed = 4.2f;
+        [SerializeField] float dwellTime = 3f;
         Fighter fighter;
         Mover mover;
         GameObject player;
         Health health;
+        NavMeshAgent navMeshAgent;
         Vector3 guardPosition;
         float timeSinceLastSawPlayer = Mathf.Infinity;
+        float timeSinceArrivedAtWaypoint = Mathf.Infinity;
+        int waypointIndex = 0;
+        float defaultSpeed;
 
         private void Start() {
             fighter = GetComponent<Fighter>();
             mover = GetComponent<Mover>();
             health = GetComponent<Health>();
             player = GameObject.FindGameObjectWithTag("Player");
+            navMeshAgent = GetComponent<NavMeshAgent>();
 
             // Initial position of the AI to remember as guard position
             guardPosition = transform.position;
+            defaultSpeed = navMeshAgent.speed;
         }
 
-        private void Update() 
+        private void Update()
         {
             if (health.IsDead()) return;
 
-            if (IsPlayerInRange()) {
-                timeSinceLastSawPlayer = 0;
+            if (IsPlayerInRange() && fighter.CanAttack(player.gameObject))
+            {
                 AttackBehaviour();
-            } 
+            }
             else if (!IsPlayerInRange() || !fighter.CanAttack(player.gameObject))
             {
                 SuspicionBehaviour();
             }
             else
             {
-                GuardBehaviour();
+                PatrolBehaviour();
             }
 
-            timeSinceLastSawPlayer += Time.deltaTime;
+            UpdateTimers();
         }
 
-        private void GuardBehaviour()
+        private void UpdateTimers()
         {
-            mover.StartMoveAction(guardPosition);
+            timeSinceLastSawPlayer += Time.deltaTime;
+            timeSinceArrivedAtWaypoint += Time.deltaTime;
+        }
+
+        private void PatrolBehaviour()
+        {
+            navMeshAgent.speed = defaultSpeed;
+            Vector3 nextPosition = guardPosition;
+
+            if (patrolPath != null)
+            {
+                if (AtWaypoint()) {
+                    timeSinceArrivedAtWaypoint = 0;
+                    CycleWaypoint();
+                }
+
+                nextPosition = GetCurrentWaypoint();
+            }
+
+            if (timeSinceArrivedAtWaypoint > dwellTime) {
+                mover.StartMoveAction(nextPosition);
+            }
+        }
+
+        private bool AtWaypoint()
+        {
+            if (Vector3.Distance(transform.position, GetCurrentWaypoint()) < waypointTolerance) {
+                return true;
+            }
+            return false;
+        }
+
+        private void CycleWaypoint()
+        {
+           waypointIndex = patrolPath.GetNextWaypoint(waypointIndex);
+        }
+
+        private Vector3 GetCurrentWaypoint()
+        {
+            return patrolPath.GetWaypoint(waypointIndex);
         }
 
         private void SuspicionBehaviour()
         {
             GetComponent<ActionScheduler>().CancelCurrentAction();
             if (timeSinceLastSawPlayer > suspicionTime) {
-                GuardBehaviour();
+                PatrolBehaviour();
             }
         }
 
         private void AttackBehaviour()
         {
-            if (fighter.CanAttack(player.gameObject)) 
-            {
-                fighter.Attack(player.gameObject);
-            }        
+            timeSinceLastSawPlayer = 0;
+            navMeshAgent.speed = attackSpeed;
+            fighter.Attack(player.gameObject);
         }
 
         private bool IsPlayerInRange()
